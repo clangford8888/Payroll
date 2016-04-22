1f056fc60b5492f930b9372d31bbc850e9a9dce2
Casey Langford <clangford8888@gmail.com>
2016-04-22 16:14:49 -0500

    Added TaskReader class

---
+++ src/payroll/tasks/TaskReader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payroll.tasks;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Casey
+ * The TaskReader class will build a list of SHS, Labor, and Equipment tasks
+ * so the task creation factories will not have to access the database directly.
+ */
+public class TaskReader {
+    
+    private Map<String, String> taskList;
+    
+    
+    
+}
